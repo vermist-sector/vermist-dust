37db70b58d9cd1df18b6a58c0d9a4e652b8ec41a
lunarcomets <140772713+lunarcomets@users.noreply.github.com>
2025-04-27 10:21:14 -0700

    admin records console can filter by record entry type again (#605)

--- Content.Client/_CD/Records/UI/CharacterRecordViewer.xaml.cs
+++ Content.Client/_CD/Records/UI/CharacterRecordViewer.xaml.cs
@@ -37,6 +37,7 @@ public sealed partial class CharacterRecordViewer : FancyWindow
     public event Action<StationRecordFilterType, string?>? OnFiltersChanged;
 
     private bool _isPopulating;
+    private bool _filtersChanged;
     private StationRecordFilterType _filterType;
 
     private RecordConsoleType? _type;
@@ -158,6 +159,7 @@ public sealed partial class CharacterRecordViewer : FancyWindow
             if (args.Id == RecordEntryViewType.SelectedId)
                 return;
             RecordEntryViewType.SelectId(args.Id);
+            _filtersChanged = true;
             // This is a hack to get the server to send us another packet with the new entries
             OnFiltersChanged?.Invoke(_filterType, RecordFiltersValue.Text);
         };
@@ -314,11 +316,11 @@ public sealed partial class CharacterRecordViewer : FancyWindow
         RecordContainerStatus.Visible = false;
         RecordContainer.Visible = true;
 
-        // DeltaV - that's not the case
-        // Do not needlessly reload the record if not needed. This is mainly done to prevent a bug in the admin record viewer.
-        // if (state.SelectedIndex == _openRecordKey)
-        //     return;
-        // _openRecordKey = state.SelectedIndex;
+        // Do not needlessly reload the record if not needed. This is mainly done to prevent a bug in the admin record viewer
+        // AND the admin record filter.
+        if (state.SelectedIndex == _openRecordKey && _filtersChanged == false)
+            return;
+        _openRecordKey = state.SelectedIndex;
 
         var record = state.SelectedRecord!;
         var cr = record.PRecords;
@@ -362,12 +364,15 @@ public sealed partial class CharacterRecordViewer : FancyWindow
                 {
                 case RecordConsoleType.Employment:
                     SetEntries(cr.EmploymentEntries, true);
+                    _filtersChanged = false;
                     break;
                 case RecordConsoleType.Medical:
                     SetEntries(cr.MedicalEntries, true);
+                    _filtersChanged = false;
                     break;
                 case RecordConsoleType.Security:
                     SetEntries(cr.SecurityEntries, true);
+                    _filtersChanged = false;
                     break;
                 }
                 break;
