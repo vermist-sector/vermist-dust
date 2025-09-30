5aecde2e4d168104ad56861ee581e1f0764f994b
Janet Blackquill <uhhadd@gmail.com>
2025-09-30 00:19:11 -0400

    Fix AllComponentsOneToOneDeleteTest

--- Content.IntegrationTests/Tests/EntityTest.cs
+++ Content.IntegrationTests/Tests/EntityTest.cs
@@ -393,6 +393,7 @@ namespace Content.IntegrationTests.Tests
                 "BiomeSelection", // Whaddya know, requires config.
                 "ActivatableUI", // Requires enum key
                 "RevealRevenantOnCollide", // Imp - Breaks because it requires physicscomp
+                "Woundable", // Offbrand - we're not doing this on its own
             };
 
             await using var pair = await PoolManager.GetServerClient();
