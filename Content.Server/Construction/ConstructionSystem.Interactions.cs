381c9454ac3cdd63304eccf5753f53b0e5cc865e
Janet Blackquill <uhhadd@gmail.com>
2025-10-11 00:56:31 -0400

    fix construction getting stuck on edges

--- Content.Server/Construction/ConstructionSystem.Interactions.cs
+++ Content.Server/Construction/ConstructionSystem.Interactions.cs
@@ -70,9 +70,15 @@ namespace Content.Server.Construction
             {
                 var result = HandleEdge(uid, ev, edge, validation, construction, out _); // Offbrand
 
-                // Reset edge index to none if this failed...
-                if (!validation && result is HandleResult.False && construction.StepIndex == 0)
-                    construction.EdgeIndex = null;
+                // Begin Offbrand
+                if (result is HandleResult.False && construction.StepIndex == 0)
+                {
+                    if (!validation)
+                        construction.EdgeIndex = null;
+
+                    return HandleNode(uid, ev, node, validation, construction);
+                }
+                // End Offbrand
 
                 return result;
             }
