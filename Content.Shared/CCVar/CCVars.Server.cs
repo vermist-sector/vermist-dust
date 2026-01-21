087cc6f181edd8e1a09fcc7ce281ae642a5e5d31
Jellvisk <telescopesd@gmail.com>
2026-01-21 11:08:17 -0500

    starting the formatting questline

--- Content.Shared/CCVar/CCVars.Server.cs
+++ Content.Shared/CCVar/CCVars.Server.cs
@@ -1,4 +1,4 @@
-﻿using Robust.Shared.Configuration;
+using Robust.Shared.Configuration;
 
 namespace Content.Shared.CCVar;
 
@@ -18,7 +18,7 @@ public sealed partial class CCVars
     ///     Guide Entry Prototype ID to be displayed as the server rules.
     /// </summary>
     public static readonly CVarDef<string> RulesFile =
-        CVarDef.Create("server.rules_file", "DefaultRuleset", CVar.REPLICATED | CVar.SERVER);
+        CVarDef.Create("server.rules_file", "VDSRuleset", CVar.REPLICATED | CVar.SERVER); // VDS ruleset
 
     /// <summary>
     ///     Guide entry that is displayed by default when a guide is opened.
